623ab60813a6d093ffdf059c33a30f829449a41d
Angel Zapata <azapata@basecampcodingacademy.org>
2017-07-26 11:56:41 -0500

    giving the costumers options they will make when purchasing a tent or tents

--- shell.py
+++ shell.py
@@ -1,6 +1,24 @@
 import time
+import core
 
 def main():
     print("\t\tWelcome to AZ's Canopy Party Tents Rental!\n")
     time.sleep(1)
-    print("Canopy tents are a light weight structure, they are great for protection from sun or light rain. They are to be installed and removed by you, the renter.")
+    print("Canopy tents are a light weight structure, they are great for protection from sun or light rain. They are to be installed and removed by you, the renter.\n")
+    time.sleep(1)
+    tent = input('Would you like to rent a tent or tents today? 1: yes 2: no')
+    if tent == '1':
+        print('Great, here are the tents we have avaliable. You can choose to just rent the tent or have chairs and tables included with them.')
+
+    
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main() 
