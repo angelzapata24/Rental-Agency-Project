7c0d3e1f73e11a962ffbe2c46ae18b894f6132ee
Angel Zapata <azapata@basecampcodingacademy.org>
2017-07-26 11:56:01 -0500

    working on functions to show the difference prizes I have for my tents

---
+++ core.py
@@ -0,0 +1,31 @@
+def type_of_tents():
+    """ str -> str
+    tent sizes 
+    """
+    tent_sizes = ['Tent 1 = 10X10',
+    'Tent 2 = 10X20',
+    'Tent 3 = 16X16',
+    'Tent 4 = 20X20',
+    'Tent 5 = 20X30',
+    'Tent 6 = 20X40']
+
+def cost_of_tents(tent):
+    ''' Prices of the avalible tents'''
+    tent1 = 50 
+    tent2 = 75
+    tent3 = 82
+    tent4 = 115
+    tent5 = 170
+    tent6 = 285
+    if items == '1':
+        return float()
+
+def tents_chair_tables(tents):
+    tent1 = 75
+    tent2 = 125
+    tent3 = 132
+    tent4 = 165
+    tent5 = 220
+    tent6 = 335
+
+
