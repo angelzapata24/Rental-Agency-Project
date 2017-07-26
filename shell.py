bdc4ce38ea0d0d67e4ccdbf4c1ac530d6f29e5d6
Angel Zapata <azapata@basecampcodingacademy.org>
2017-07-26 10:47:14 -0500

    I've added my welcome and a little describtion of canopy tents as print()

---
+++ shell.py
@@ -0,0 +1,6 @@
+import time
+
+def main():
+    print("\t\tWelcome to AZ's Canopy Party Tents Rental!\n")
+    time.sleep(1)
+    print("Canopy tents are a light weight structure, they are great for protection from sun or light rain. They are to be installed and removed by you, the renter.")
