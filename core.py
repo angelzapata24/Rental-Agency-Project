69242e7ed70ea58a5f83a1178b67bfa72b3f726a
Angel Zapata <azapata@basecampcodingacademy.org>
2017-07-27 16:23:26 -0500

    my functions that gives me my sales tax and deposit works now, I also gave all my functions tests

--- core.py
+++ core.py
@@ -1,6 +1,8 @@
 def type_of_tents():
     """ str -> str
     tent sizes 
+    >>> type_of_tents()
+    ['Tent 1 = 10X10\\t\\tTent 7 = 10X10 w/ 6 chairs & 1 table', 'Tent 2 = 10X20\\t\\tTent 8 = 10X20 w/ 15 chairs & 3 tables', 'Tent 3 = 16X16\\t\\tTent 9 = 10X20 w/ 20 chairs & 4 tables', 'Tent 4 = 20X20\\t\\tTent 10 = 10X20 w/ 40 chairs & 6 tables', 'Tent 5 = 20X30\\t\\tTent 11 = 10X20 w/ 55 chairs & 7 tables', 'Tent 6 = 20X40\\t\\tTent 12 = 10X20 w/ 70 chairs & 8 tables']
     """
     tent_sizes = ['Tent 1 = 10X10\t\t' 'Tent 7 = 10X10 w/ 6 chairs & 1 table',
     'Tent 2 = 10X20\t\t' 'Tent 8 = 10X20 w/ 15 chairs & 3 tables',
@@ -13,10 +15,10 @@ def type_of_tents():
 def cost_of_tents(tent):
     ''' string -> string 
     Prices of the avalible tents
-    >>> cost_of_tents(1)
+    >>> cost_of_tents('1')
     50
-    >>> cost_of_tents(12)
-    330    
+    >>> cost_of_tents('12')
+    335
     '''
     tent1 = 50   
     tent2 = 75
@@ -29,7 +31,7 @@ def cost_of_tents(tent):
     tent9 = 132
     tent10 = 165
     tent11 = 220
-    tent12 = 330
+    tent12 = 335
 
     if tent == '1' or tent.lower() == 'one':
         return tent1 
@@ -59,16 +61,18 @@ def cost_of_tents(tent):
 def deposit(tents):
     """deposit for every tent
     >>> deposit(50)
-    55.0
+    5.0
     >>> deposit(75)
-    82.5
+    7.5
     """
-    return (float(tents) * float(.1)) + float(tents)
+    tent = (float(tents) * float(0.10))
+    return round(tent, 2)
 
 def total(tents):
     """ returns the total price of a tent, tent price plus
     deposit plus sales tax.
     >>> total(50)
-    53.5
+    3.5
     """
-    return (float(tents) * float(.07)) + float(tents)
+    tent = (float(tents) * float(0.07))
+    return round(tent, 2)
