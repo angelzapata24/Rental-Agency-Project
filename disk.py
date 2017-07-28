eb1a951481077426f85b49e24dfc33a51525d60f
Angel Zapata <azapata@basecampcodingacademy.org>
2017-07-28 13:59:49 -0500

    added two functions that are supposed to get my inventory working

--- disk.py
+++ disk.py
@@ -15,3 +15,30 @@ def in_history():
         left.append([split_string[0], float(split_string[1].strip().replace('$', ''))])
     return left
  
+def in_inventory():
+    left = []
+    with open('inventory.txt', 'r') as file:
+        file.readline()
+        lines = file.readlines()
+    for line in lines:
+        split_string = line.strip().split(', ')
+        left.append([(split_string[0]), (split_string[1])])
+    return left
+
+def take_away(tent, size):
+    str_l = ['tent, size']
+    left = in_inventory()
+    for item in left:
+        if item == tent:
+            if float(amount) > float(item[1]):
+                return False
+            else:
+                item[1] = float(item[1]) - float(amount)
+        item[0] = str(item[0])
+        item[1] = str(item[1])
+        str_l.append(', '.join(item))
+        message = '\n'.join(str_l)
+
+    with open('inventory.txt', 'w') as file: 
+        file.write(message)
+    return True
