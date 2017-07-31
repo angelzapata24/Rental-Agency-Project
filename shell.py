cbabb05267c7f2013b67e492bb458efba2c598b1
Angel Zapata <azapata@basecampcodingacademy.org>
2017-07-31 13:18:21 -0500

    I added a input where you can say how many tents you want to rent and it will take away the tents from inventory.

--- shell.py
+++ shell.py
@@ -1,12 +1,20 @@
 import time
 import core
 import disk
+import time,sys
+
+typing_speed = 15
+def slow_type(t):
+    for l in t:
+        sys.stdout.write(l)
+        sys.stdout.flush()
+        time.sleep(typing_speed / 970.0)
+    return input()
 
 def main():
     print("\t\tWelcome to AZ's Canopy Party Tents Rental!\n")
-    time.sleep(.5)
-    print("Canopy tents are a light weight structure, they are great for protection from sun or light rain. They are to be installed and removed by you, the renter.\n")
-    time.sleep(5)
+    time.sleep(1.5)
+    slow_type("Canopy tents are a light weight structure, they are great for protection from sun or light rain. They are to be installed and removed by you, the renter. Press 'Enter' to continue.\n")
     tent = input('Canopy tents are available to rent up to 2 days. Would you like to rent a tent or tents today? 1: yes 2: no\n')
     if tent == '2' or tent.lower() == 'no':
         print("That's fine, come back again if you change your mind!")
@@ -16,37 +24,31 @@ def main():
             print('\n'.join(core.type_of_tents()))
             tent1 = input('Tent: \n')
             not1 = core.cost_of_tents(tent1)
-            if not disk.take_away(tent1, not1):
-                print('sorry')
-                return None
             tents = core.cost_of_tents(tent1)
-            con = input('\nTent ' + tent1 + ' price is $' + str(tents) + ' dollars. Would you like to continue?\n')
-            tent2 = core.deposit(tents)
-            tent3 = core.total(tents)
-            tent4 = tents * 2
+            amount = input('\n How many tents of this type would you like?')
+            tent_1 = float(amount) * float(tents)
+            if not disk.take_away(tent1, amount):
+                print('Sorry, we do not have this type of tent available.')
+                return None
+            con = input('\nTent ' + tent1 + ' price is $' + str(tent_1) + ' dollars. Would you like to continue?\n')
+            tent2 = core.deposit(tent_1)
+            tent3 = core.total(tent_1)
+            tent4 = tent_1 * 2
             if con == 'yes':
                 days = input('\nHow many days will you be renting this? Only up to 2 days.\n')
-                total = (float(tent2) + float(tent3)) + float(tents)
+                total = (float(tent2) + float(tent3)) + float(tent_1)
                 total2 = (float(tent2) + float(tent3)) + float(tent4)
                 if days == '1':
                     print('Your renting this for 1 day, your total with deposit and tax is $' + str(float(total)))
                     history = disk.keep_history(tent1, total)
                     print('Thank you')
                 elif days == '2':
-                    print('\nYour renting this for 2 day, your total with deposit and tax is $' + str(float(total2)))
+                    print('\nYour renting this for 2 days, your total with deposit and tax is $' + str(float(total2)))
                     history = disk.keep_history(tent1, total2)
                     print('Thank you')
                     
 
         
-
-
-
-    
-
-
-
-
 
 
 
